289467a30295091abf042dfd797b41d6e610f301
Grupa 1 <grupa1@administrators-iMac-10.local>
2019-11-20 17:51:51 +0100

    JS backup

---
+++ script.js
@@ -0,0 +1,29 @@
+document.write("Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptatum dignissimos distinctio inventore quasi nostrum unde dicta harum, eius repudiandae natus perferendis rem at laborum voluptatem, quo explicabo voluptas pariatur!");
+document.write('<br/>');
+document.write('<br/>');
+document.write('<br/>');
+document.write('<br/>');
+
+
+// console.log("ovo je ispis na konzolu")
+// console.error("ovo je ispis na konzolu")
+
+// var userName = "Haris";
+// var age = 20;
+// var learningJS = true;
+
+// console.log(userName);
+// console.log(age);
+// console.log(learningJS);
+
+// var number1 = parseInt (prompt("Enter first number: "));
+// var number2 = parseInt (prompt("Enter second number: "));
+// var result = number1 + number2;
+
+// console.log("Prvi broj: " + number1);
+// console.log("Drugi broj: " + number2);
+// console.log("Rezultat je: " + result);
+
+var text = "Today is a good day";
+console.log(text);
+console.log(typeof(text));
